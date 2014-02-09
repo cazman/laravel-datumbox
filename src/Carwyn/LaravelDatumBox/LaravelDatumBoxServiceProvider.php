4d490883818152f3adcfe5bb706e95c923d24f4d
Carwyn Stephen <caztephen@gmail.com>
2014-02-09 19:28:21 +0000

    initial commit try 2

---
+++ src/Carwyn/LaravelDatumBox/LaravelDatumBoxServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Carwyn\LaravelDatumBox;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelDatumBoxServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('carwyn/laravel-datum-box');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
